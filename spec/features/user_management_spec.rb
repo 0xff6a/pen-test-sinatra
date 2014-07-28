c3eb79e48328f690238aefe972f87db36a7c0ba3
Jeremy Fox <foxjerem@gmail.com>
2014-07-28 16:47:14 +0100

    Added password validation

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -8,12 +8,18 @@ feature 'User signs up' do
 		expect(User.first.email).to eq('jeremy@example.com')
 	end
 
+	scenario 'with a password that does not match' do
+		expect { sign_up('a@a.com', 'pass', 'wrong') }.to change(User, :count).by(0)
+	end
+
 	def sign_up(email = 'jeremy@example.com',
-							password = '1234')
+							password = '1234',
+							password_confirmation = '1234')
 		visit 'users/new'
 		expect(page.status_code).to eq 200
 		fill_in :email, :with => email
 		fill_in :password, :with => password
+		fill_in :password_confirmation, :with => password_confirmation
 		click_button 'Sign Up'
 	end
 
