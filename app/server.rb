9f6088caa1494afa803b2b35feae74e26a079139
Jeremy Fox <foxjerem@gmail.com>
2014-07-29 11:06:08 +0100

    Extracted flash and welcome message to partial files

--- app/server.rb
+++ app/server.rb
@@ -2,11 +2,13 @@ require 'sinatra'
 require 'sinatra/partial'
 require 'data_mapper'
 require 'rack-flash'
-require File.join(File.dirname(__FILE__), '..', 'lib/link.rb')
-require File.join(File.dirname(__FILE__), '..', 'lib/tag.rb')
-require File.join(File.dirname(__FILE__), '..', 'lib/user.rb')
+
+require_relative 'models/link'
+require_relative 'models/tag'
+require_relative 'models/user'
 require_relative 'helpers/application'
 require_relative 'data_mapper_setup'
+
 require_relative 'controllers/application.rb'
 require_relative 'controllers/links.rb'
 require_relative 'controllers/sessions.rb'
@@ -16,6 +18,7 @@ require_relative 'controllers/users.rb'
 enable :sessions
 set :session_secret, 'supercalifragalisticexpialodocious'
 use Rack::Flash
+set :partial_template_engine, :erb
 
 
 
