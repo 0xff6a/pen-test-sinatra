6844c6b603e385dfe7b5f3a727de98fd33da3c8a
Jeremy Fox <foxjerem@gmail.com>
2014-07-31 13:53:10 +0100

    Added tag tests

--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,11 +1,21 @@
 describe Tag do
 
+	before(:each) do
+		user = create_user('test@test.com', 'test')
+	end
+
 	it 'there are no tags initially' do
 		expect(Tag.count).to eq(0)
 	end
 
-	it 'has multiple text fields' do
-		# Tag.create
+	it 'can be created' do
+		Tag.create(:text => 'education', :user_id => user.id)
+		expect(Tag.count).to eq(1)
+	end
+
+	it 'must have text to be created' do
+		Tag.create(:text => nil, :user_id => user.id)
+		expect(Tag.count).to eq(0)
 	end
 	
 end
